776386b19b66952a1766112c3dd136fa975a83a4
danieltinois <danieltinois@gmail.com>
2025-02-17 15:34:29 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -46,19 +46,24 @@ const styles = {
 const App = () => {
   const [gerarImg, setGerarImg] = useState(null);
   const [mostrarConfeti, setMostrarConfeti] = useState(false);
+  const [carregando, setCarregando] = useState(false);
 
   async function handleGerarImagem() {
+    setCarregando(true);
     const response = await fetch('https://dog.ceo/api/breeds/image/random');
     const json = await response.json();
 
     setGerarImg(json.message);
 
-    const audio = new Audio(notification);
-    audio.play();
+    setTimeout(() => {
+      const audio = new Audio(notification);
+      audio.play();
 
-    setMostrarConfeti(true);
+      setMostrarConfeti(true);
 
-    setTimeout(() => setMostrarConfeti(false), 3000);
+      setTimeout(() => setMostrarConfeti(false), 3000);
+      setCarregando(false);
+    }, 500);
   }
   return (
     <div style={styles.container}>
@@ -68,7 +73,7 @@ const App = () => {
         </div>
       )}
       <button onClick={handleGerarImagem} style={styles.button}>
-        Clique aqui e não se arrependa!
+        {carregando ? 'Carregando...' : 'Clique aqui e não se arrependa!'}
       </button>
       {gerarImg && <img style={styles.imagem} src={gerarImg} />}
     </div>
